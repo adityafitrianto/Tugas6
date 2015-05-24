795c2ff7faa9c1cb9344aed01f256279dacb853f
Rikiandri <rikiandrean81@gmail.com>
2015-05-25 00:48:18 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/unsyiah/elektro/mobile/ryan/tugas6/layout2.java
@@ -0,0 +1,43 @@
+package id.ac.unsyiah.elektro.mobile.ryan.tugas6;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class layout2 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout2);
+
+        Intent intentLy2 = getIntent();
+        String ly2Str = intentLy2.getStringExtra("LyStr");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_layout2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
